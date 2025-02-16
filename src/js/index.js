e24c57506ff6e69fd8bfbe32fec9a85c70aa2c7b
Yasmim Coimbra <ymdsc.coimbra85@gmail.com>
2025-02-16 18:05:02 -0300

    JS: identifying the DOM elements i want to manipulate

---
+++ src/js/index.js
@@ -0,0 +1,3 @@
+const camposObrigatorios = document.querySelectorAll('.campo-obrigatorio');
+const btnForm = document.getElementById('form-btn');
+const alertaErro = document.querySelectorAll('.error-msg');
